7c44e6e93b3423f9313034acf15c853193e69a26
Muiz Uvais <muizuvais99@gmail.com>
2020-07-14 23:14:20 +0530

    Added autostart bash script

---
+++ .config/redshift/autostart.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+#Terminate already running bar instances
+killall -q redshift
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+
+# Launch Redshift, using default config location ~/.config/redshift/redshift.conf
+redshift &
+
+echo "Redshift Launched.. "
