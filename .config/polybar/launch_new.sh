c20a3d80356e6472ddb76108985f0bce97b6cff7
MuizU <muizuvais99@gmail.com>
2020-08-03 13:24:39 +0530

    Added polybar

--- .config/polybar/launch_new.sh
+++ .config/polybar/launch_new.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,9 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload bar &
+# Launch Polybar, using default config location ~/.config/polybar/config
+for screen in $(bspc query -M --names)
+do
+    polybar $screen &
 done
 
-#polybar --reload bar &
-echo "Polybars launched..."
+echo "Polybar launched..."
